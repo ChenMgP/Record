b7a11b6d0b7f5866c667324d932975604a632a1f
NaiBai <93812337+ChenMgP@users.noreply.github.com>
2022-05-15 14:17:02 +0800

    Add files via upload

--- src/main/java/com/kcn/Main.java
+++ src/main/java/com/kcn/Main.java
@@ -8,20 +8,28 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
-    public Main() {
-    }
+    //The constructor isn't important,Ignore it!
+    public Main() {}
+    //The method isn't important,Ignore it!
+    public static void main(String[] args) {}
 
-    public static void main(String[] args) {
-
-    }
-
+    //Create you sound identifier
     public static final Identifier MY_SOUND_ID = new Identifier("kcn:my_sound");
+    //Create you sound event
     public static SoundEvent MY_SOUND_EVENT = new SoundEvent(MY_SOUND_ID);
-    public static final MyRecord MY_RECORD = new MyRecord(4, MY_SOUND_EVENT, new Item.Settings().group(ItemGroup.MISC));
+    /*Create you Record
+    1.The first formal parameter describes the intensity of the redstone signal that the record can emit when placed in the jukebox
+    2.The second formal parameter is your sound event
+    3.The third formal parameter you don't have to think about it,copy it unless you want to change something
+     */
+    public static final MyRecord MY_RECORD = new MyRecord(4, MY_SOUND_EVENT, new Item.Settings().group(ItemGroup.MISC).maxCount(1));
 
     @Override
     public void onInitialize() {
+        //Register your sound event and record
         Registry.register(Registry.ITEM, new Identifier("kcn", "my_record"), MY_RECORD);
         Registry.register(Registry.SOUND_EVENT, MY_SOUND_ID, MY_SOUND_EVENT);
+        //If you have completed the above,Under the "resources/assets/kcn" path, create the sounds folder and sounds.json file
+        //After that,you will get a record of your own
     }
 }
--- src/main/java/com/kcn/MyRecord.java
+++ src/main/java/com/kcn/MyRecord.java
@@ -3,6 +3,10 @@ package com.kcn;
 import net.minecraft.item.MusicDiscItem;
 import net.minecraft.sound.SoundEvent;
 
+/*
+    The reason for creating this class is to get the constructor of MusicDiscItem
+    Because the constructor of this class is protected
+ */
 public class MyRecord extends MusicDiscItem {
     public MyRecord(int comparatorOutput, SoundEvent sound, Settings settings) {
         super(comparatorOutput, sound, settings);
